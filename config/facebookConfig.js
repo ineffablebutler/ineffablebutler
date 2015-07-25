1235b9032e6a688412e9a6b97e420676804b974b
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-25 14:08:56 -0700

    (feat) Add footer to bottom of home page

--- config/facebookConfig.js
+++ config/facebookConfig.js
@@ -2,8 +2,8 @@
 //facebook clientId and Secret from https://developers.facebook.com/apps
 //This file is used with /config/routes/auth.js to authenticate FB users
 var facebookConfig = {
-    clientID: '130802563917598',
-    clientSecret: '7a1bb563a6dfebb5c4992bf21470d5ad',
+    clientID: '1684524431776154',
+    clientSecret: '469dc8ad6b2fc3c55881ffa410471d6a',
     callbackURL: "http://munib.herokuapp.com/auth/facebook/callback",
     enableProof: false
   };
--- config/googleConfig.js
+++ config/googleConfig.js
@@ -1,8 +1,8 @@
 
 //google clientId and clientSecret made from https://console.developers.google.com/project
 var googleConfig = {
-    clientID: '189160676278-ci6rhq98n64ig2ekbb4ojst990u6tg6s.apps.googleusercontent.com',
-    clientSecret: 'SkWnFwdTOG69OgDZi092hHqC',
+    clientID: '926781525836-h1hh7ekpram4koj9c07pq34rr0bgu90b.apps.googleusercontent.com',
+    clientSecret: 'FJXk2SYLWYSO1nebxMs6el9d',
     callbackURL: "http://munib.herokuapp.com/auth/google/callback"
   };
 
